33603fb1f88a64beebec77e32b4774c2a585c897
Lucy Linder <lucy.derlin@gmail.com>
2024-03-21 17:19:16 +0100

    fix: add author and version information to package

--- mantelo/__init__.py
+++ mantelo/__init__.py
@@ -1,3 +1,9 @@
+from .version import __version__  # noqa: F401
+
+
+__author__ = "Lucy Linder"
+__email__ = "lucy.derlin@gmail.com"
+
 from .client import KeycloakAdmin
 from .exceptions import AuthenticationException
 
